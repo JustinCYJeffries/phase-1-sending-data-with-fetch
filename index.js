eb7c4846ddf047f4216c1b7a0b3d42d50322b2e4
Justin Jeffries <justincyjeffries@gmail.com>
2022-02-20 22:48:28 -0700

    commit tree 0703cd5d4a58ccfea45ff985dc9a02738ba8b534

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Add your code here
+
+  
+  // method: "POST" is missing from the object below
+  
+  
+  let body = document.querySelector('body')  
+
+  function submitData(firstName, email){
+    const configurationObject = {
+        method: "Post",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify({
+            "name" : firstName,
+            "email": email
+          }),
+      };
+    return fetch("http://localhost:3000/users", configurationObject )
+    .then(response => response.json())
+    .then(body => renderID(body.id))
+    .catch(function response(body){
+    let message = "Unauthorized Access"
+    document.querySelector('body').innerHTML = message;
+    })
+     };
+    
+    function renderID(user){
+        document.querySelector('body').innerHTML = user;
+    }
+    
+    
+    submitData()
+
+
+
+
+
